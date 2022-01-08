b4aede40e307db90712f5250dd135f34f90682dd
teo3fl <teo3fl@gmail.com>
2022-01-09 00:25:13 +0200

    Added airplane trail management

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -11,12 +11,17 @@ public class Airplane : MonoBehaviour
     [SerializeField]
     private float f_descendRate = 0.5f;
 
+    [SerializeField]
+    private GameObject go_workingTrailRenderers;
+    [SerializeField]
+    private GameObject go_damagedTrailRenderers;
+
     private bool isHit = false;
     Vector3 ground = new Vector3(1, 0, 1);
 
     void Update()
     {
-        if(isHit)
+        if (isHit)
         {
             Vector3 targetDirection = ground - transform.position;
             var angle = Vector3.Angle(targetDirection, transform.forward) % 91f;
@@ -34,14 +39,19 @@ public class Airplane : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.GetComponent<Rocket>() != null)
+        if (!isHit && other.gameObject.GetComponent<Rocket>() != null)
         {
             isHit = true;
             Debug.Log("Plane was hit by rocket");
+            go_workingTrailRenderers.transform.DetachChildren();
+            go_damagedTrailRenderers.SetActive(true);
         }
         else
         {
             Debug.Log("Plane hit bounds");
+            go_workingTrailRenderers.transform.DetachChildren();
+            if (go_damagedTrailRenderers.activeInHierarchy)
+                go_damagedTrailRenderers.transform.DetachChildren();
             Destroy(gameObject);
         }
     }
