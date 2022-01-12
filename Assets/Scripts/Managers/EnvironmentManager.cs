c085adc228b72c8bf9e8ecda649e3c499d7c514b
teo3fl <teo3fl@gmail.com>
2022-01-13 01:38:39 +0200

    Added functionality for switching between nighttime and daytime environments

---
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    [SerializeField]
+    private Material daySkybox;
+    [SerializeField]
+    private Material nightSkybox;
+    [SerializeField]
+    private Light sceneDirectionalLight;
+    [SerializeField]
+    private float f_daylightIntensity = 2;
+    [SerializeField]
+    private float f_nightlightIntensity = 0.5f;
+
+    private bool isDay = true;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            isDay = !isDay;
+            UpdateEnvironment();
+        }
+    }
+
+    private void UpdateEnvironment()
+    {
+        if(isDay)
+        {
+            RenderSettings.skybox = daySkybox;
+            sceneDirectionalLight.intensity = f_daylightIntensity;
+        }
+        else
+        {
+            RenderSettings.skybox = nightSkybox;
+            sceneDirectionalLight.intensity = f_nightlightIntensity;
+        }
+    }
+}
