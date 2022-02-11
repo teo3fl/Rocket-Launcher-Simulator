7ed4b6b732f95af081928a47c10df968be682869
teo3fl <teo3fl@gmail.com>
2022-02-11 14:06:40 +0200

    Fixed early rocket destruction caused by auxiliary collider size

--- Assets/Scripts/Objects/AuxilliaryCollider.cs
+++ Assets/Scripts/Objects/AuxilliaryCollider.cs
@@ -7,9 +7,25 @@ public class AuxilliaryCollider : MonoBehaviour
     [SerializeField]
     private Rocket rocket;
 
+    private BoxCollider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        StartCoroutine(ManageCollider());
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("rocket hit terrain");
         rocket.SelfDestruct();
+    }
+
+
+    private IEnumerator ManageCollider()
+    {
+        collider.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        collider.enabled = true;
     }
 }
